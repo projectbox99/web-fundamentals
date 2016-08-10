76d85767d2b22b3375aaf6f9b01ae78cddb22ea6
anapac <anapac@gmx.us>
2016-08-10 18:48:57 +0300

    vendor.ts

--- app/vendor.ts
+++ app/vendor.ts
@@ -1,3 +1,18 @@
-import "core-js/es6";
-import "zone.js/dist/zone";
-import "reflect-meta/Reflect.js";
+// import "core-js/es6";
+// import "zone.js/dist/zone";
+// import "reflect-meta/Reflect.js";
+
+import 'es6-shim';
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+
+import "rxjs";
+
+// if ('production' === 'BRUNCH_ENVIRONMENT') {
+//   let ngCore = require('@angular/core');
+//   ngCore.enableProdMode();
+// }
